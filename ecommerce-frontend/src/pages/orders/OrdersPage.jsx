1907b4c2a43670a85fb401db5052cefc7d428212
Jonatan Valen <102178908+jvalen01@users.noreply.github.com>
2025-07-31 11:10:01 +0200

    Make create order button work

--- ecommerce-frontend/src/pages/orders/OrdersPage.jsx
+++ ecommerce-frontend/src/pages/orders/OrdersPage.jsx
@@ -2,7 +2,6 @@ import "./OrdersPage.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
-import { Link } from "react-router";
 import { OrdersGrid } from "./OrdersGrid";
 
 export function OrdersPage({ cart }) {
@@ -26,35 +25,6 @@ export function OrdersPage({ cart }) {
       <link rel="icon" type="image/svg+xml" href="/orders-favicon.svg" />
 
       <Header cart={cart} />
-
-      <div className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
-      </div>
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
